249f6168bac5946b6219731f301fc8b7f61bd92c
oluwamuibiclear <igbokweudoka@gmail.com>
2024-09-04 23:18:48 -0500

    Make HotCollection Dynamic

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+
+import Carousel from "../UI/Carousel";
+import axios from "axios";
+import HotCollectionSkeleton from "../UI/HotCollectionSkeleton";
 
 const HotCollections = () => {
+  const [hotCollections, setHotCollections] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  async function fetchCollection() {
+    try {
+      const { data } = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`)
+      setHotCollections(data)
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+    finally {
+      setIsLoading(false)
+    }
+
+
+  }
+  useEffect(() => {
+
+    fetchCollection()
+  }, [])
+  // isLoading && console.log(123)
+  // !isLoading && console.log(hotCollections)
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,29 +38,11 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
-              </div>
-            </div>
-          ))}
+          {
+            isLoading ? (<HotCollectionSkeleton/> ): (<Carousel hotCollections={hotCollections} />)
+          }
+          {/* {isLoading ? (): } */}
+
         </div>
       </div>
     </section>
