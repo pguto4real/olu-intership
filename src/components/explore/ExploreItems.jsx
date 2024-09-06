3068fea2e8f5f7e346d59870e3ea664c07d44f1e
oluwamuibiclear <igbokweudoka@gmail.com>
2024-09-05 21:04:56 -0500

    Make Explore Page dynamin and sortable

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,78 +1,94 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import { DataContext } from "../../Helper/Context";
+import GetData from "../../Hooks/GetData";
+import AutorCard from "../../Common/AuthorCard";
+import NewItemSkeleton from "../UI/NewItemSkeleton";
 
 const ExploreItems = () => {
+  const { exploreData, setExploreData } = useContext(DataContext);
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [filter, setFilter] = useState("");
+  const [visibleCount, setVisibleCount] = useState(8); // Initially show 8 items
+  const [visibleItems, setVisibleItems] = useState(
+    exploreData.slice(0, visibleCount)
+  );
+
+  const { fetchData } = GetData();
+
+  // Handler for the "Load More" button
+  const handleLoadMore = () => {
+    
+    const newCount = visibleCount + 4; // Load 4 more items on each click
+    setVisibleCount(newCount);
+    setVisibleItems(exploreData.slice(0, newCount));
+  };
+
+  const handleInputChange = (e) => {
+    setFilter(e.target.value);
+    fetchNewItemData(e.target.value);
+  };
+
+  const fetchNewItemData = async (filterData) => {
+    setIsLoading(true);
+
+    let dataFrom;
+    filterData === ""
+      ? (dataFrom = `explore`)
+      : (dataFrom = `explore?filter=${filterData}`);
+    const data = await fetchData({ dataFrom: dataFrom });
+    if (filterData === "") {
+      setExploreData(data);
+      setVisibleCount(4);
+      setVisibleItems(data.slice(0, 8));
+    } else {
+      setExploreData(data);
+      setVisibleCount(data.length);
+      setVisibleItems(data.slice(0, data.length));
+    }
+  
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchNewItemData(filter);
+  }, []);
+
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select id="filter-items" defaultValue="" onChange={handleInputChange}>
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to="/author"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={AuthorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            <div className="de_countdown">5h 30m 32s</div>
-
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
-            </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
-              </Link>
-              <div className="nft__item_price">1.74 ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>69</span>
-              </div>
-            </div>
-          </div>
+   
+      {isLoading && (
+        <>
+          <NewItemSkeleton />
+          <NewItemSkeleton />
+        </>
+      )}
+      {exploreData.length > 0 &&
+        !isLoading &&
+        visibleItems.map((explore, index) => (
+          <AutorCard key={index} data={explore} addDesign={true} />
+        ))}
+      {!isLoading && visibleCount < exploreData.length && (
+        <div className="col-md-12 text-center">
+          <Link
+            to=""
+            id="loadmore"
+            className="btn-main lead"
+            onClick={handleLoadMore}
+          >
+            Load more
+          </Link>
         </div>
-      ))}
-      <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
-      </div>
+      )}
     </>
   );
 };
